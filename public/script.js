8dc921f915dca5831a9648a421f21b01f792d593
ridaiqbal <ridariqbal@gmail.com>
2023-06-06 19:28:30 -0400

    analytics sans fav exercise without console.logs

--- public/script.js
+++ public/script.js
@@ -26,20 +26,16 @@ const exercises = [
 
 
 exercises.forEach(element => {
-    console.log(element.value)
     totalTarget = totalTarget + element.value
 })
-console.log(totalTarget)
 
 let completed = totalMinutes/totalTarget*100
 document.getElementById('completed').innerHTML = completed + '%'
 
 exercises.forEach(element => {
     const btn = document.getElementById(element.id)
-console.log(btn, "btn")
 
     btn.addEventListener("click", function() {
-        console.log("i've been clicked", btn.classList)
         
         if(btn.classList.value == 'far fa-circle') {
             
@@ -48,7 +44,7 @@ console.log(btn, "btn")
             
             totalMinutes = totalMinutes + element.value
             let completed = totalMinutes/totalTarget*100
-            console.log(totalMinutes)
+            
             document.getElementById('totalMinutes').innerHTML = totalMinutes
             document.getElementById('completed').innerHTML = completed + '%'
 
